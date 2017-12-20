f67eed6a1b79a63ecca3219f884966f897e376f0
joniles <jon.iles@procore.com>
2017-12-20 13:10:55 +0000

    Initial revision

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# graphicsmagick-packager
+# GraphicsMagick Ubuntu Packager
+
+The most recent [GraphicsMagick](http://www.graphicsmagick.org/) package for Ubuntu is 1.3.18 which dates back to 2013.
+
+The `build.sh` script here takes a [GraphicsMagick source distribution](https://sourceforge.net/projects/graphicsmagick/files/graphicsmagick/), configures, builds and packages it.
+
+To build a new packaged version:
+
+1. Copy the GraphicsMagick source `.tar.gz` archive to this directory (e.g. `GraphicsMagick-1.3.27.tar.gz`)
+2. Run `build.sh`.
+
+To install the packaged version that this script creates, use:
+
+```
+sudo dpkg -i ./graphicsmagick_1.3.27_amd64.deb
+```
+
+NOTE:
+1. The build script assumes that [fpm](https://rubygems.org/gems/fpm) is installed.
+2. The build configuration was taken from the 1.3.18 package (the `gm version` command line shows build configuration used).
---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+GM_SOURCE_ARCHIVE=$(ls GraphicsMagick*.tar.gz)
+GM_SOURCE=${GM_SOURCE_ARCHIVE%.tar.gz}
+GM_VERSION=${GM_SOURCE#*-}
+
+TARGET_DIR=$PWD/source
+mkdir $TARGET_DIR
+
+echo "Extracting source from $GM_SOURCE_ARCHIVE"
+tar xzf $GM_SOURCE_ARCHIVE
+
+echo "Configuring $GM_SOURCE"
+cd $GM_SOURCE
+./configure  '--build' 'x86_64-linux-gnu' '--disable-installed' '--disable-shared' '--enable-libtool-verbose' --prefix=$TARGET_DIR '--mandir=${prefix}/share/man' '--infodir=${prefix}/share/info' '--docdir=${prefix}/share/doc/graphicsmagick' '--with-gs-font-dir=/usr/share/fonts/type1/gsfonts' '--with-x' '--x-includes=/usr/include/X11' '--x-libraries=/usr/lib/X11' '--without-dps' '--without-modules' '--without-frozenpaths' 'build_alias=x86_64-linux-gnu' 'CFLAGS=-Wall -g -fno-strict-aliasing  -fPIE  -fstack-protector --param ssp-buffer-size=4  -D_FORTIFY_SOURCE=2  -Wformat -Wformat-security -Werror=format-security  -O2' 'LDFLAGS=  -fPIE -pie  -Wl,-z,relro  -Wl,-z,now ' 'CXXFLAGS=-Wall -g -fno-strict-aliasing  -fPIE  -fstack-protector --param ssp-buffer-size=4  -D_FORTIFY_SOURCE=2  -Wformat -Wformat-security -Werror=format-security  -O2'
+
+echo "Building $GM_SOURCE"
+make install
+cd ..
+
+echo "Packaging $GM_SOURCE"
+rm -f *.deb
+fpm -s dir -t deb -C $TARGET_DIR --prefix /usr -n graphicsmagick -v $GM_VERSION
+
+echo "Cleaning up"
+rm -rf $TARGET_DIR
+rm -rf $GM_SOURCE
